f768275e6d96c90b869c9656c8e04bb77c6d56f9
ganeshkumar5412-ux <ganeshkumar5412@gmail.com>
2025-12-24 14:58:28 +0530

    updated with personlizations

--- lab1/versions.tf
+++ lab1/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "ganeshkumar5412-org"
     workspaces {
       name = "ace-automation-lab1"
     }
